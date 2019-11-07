c6c05eadadadb8f1cb8f0d84fb27cae9122befa9
illutek <stefanvandenborne@gmail.com>
2019-11-07 21:30:23 +0100

    Add a custom PTS theme

---
+++ web/themes/custom/pts/js/menu.js
@@ -0,0 +1,36 @@
+/**
+ * Created by stefan on 01-11-2019.
+ *
+ */
+
+(function () {
+    'use strict';
+  
+    const openBtn = document.querySelector('.hamburger__button');
+    const closeBtn = document.querySelector('.close__button');
+    const menuOverlay = document.querySelector('.menu-overlay');
+    const navTopHidden = document.querySelector('.nav--top');
+    const openmenu = document.querySelector('.menu-overlay');
+  
+    // console.log(closeBtn);
+  
+    /** 
+     *  Functions
+    */
+  
+    function closeMenu() {
+      menuOverlay.classList.remove('menu-overlay--visible');
+      navTopHidden.classList.remove('nav--hidden');
+    }
+  
+    function openMenu() {
+      menuOverlay.classList.toggle('menu-overlay--visible');
+      navTopHidden.classList.add('nav--hidden');
+    }
+  
+    openBtn.addEventListener('click', openMenu);
+  
+    closeBtn.addEventListener('click', closeMenu);
+    openmenu.addEventListener('click', () => setTimeout(closeMenu, 1000));
+  
+  })();
---
+++ web/themes/custom/pts/readme.md
@@ -0,0 +1,8 @@
+## PTS aanhangwagenbouw
+To convert these work files to a Drupal8 theme, perform the following steps
+
+1. clone the repo
+2. run npm install
+3. run gulp deploy
+
+All this creates the files necessary for a Drupal8 theme.
